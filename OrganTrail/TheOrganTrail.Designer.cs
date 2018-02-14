38fe59e3b0256eb748a2eb9defe6093fa15b7a03
Noble115 <jimmyphamho@yahoo.com>
2018-02-14 13:33:45 -0600

    Game 1

---
+++ OrganTrail/TheOrganTrail.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace OrganTrail
+{
+    partial class TheOrganTrail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtChoice = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(38, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(595, 76);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "The Organ Trail";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Location = new System.Drawing.Point(171, 112);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(298, 120);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "You may:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 22);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "3. Exit";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(251, 22);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "2. Learn about the body";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "1. Travel the body";
+            // 
+            // txtChoice
+            // 
+            this.txtChoice.BackColor = System.Drawing.SystemColors.MenuText;
+            this.txtChoice.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtChoice.Font = new System.Drawing.Font("Poor Richard", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtChoice.ForeColor = System.Drawing.SystemColors.Menu;
+            this.txtChoice.Location = new System.Drawing.Point(230, 292);
+            this.txtChoice.MaxLength = 1;
+            this.txtChoice.Name = "txtChoice";
+            this.txtChoice.Size = new System.Drawing.Size(137, 17);
+            this.txtChoice.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Stencil", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(48, 292);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(176, 18);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "What is your choice?";
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.BackColor = System.Drawing.Color.YellowGreen;
+            this.lblTest.Location = new System.Drawing.Point(558, 173);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(35, 13);
+            this.lblTest.TabIndex = 4;
+            this.lblTest.Text = "label6";
+            // 
+            // TheOrganTrail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(708, 378);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtChoice);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "TheOrganTrail";
+            this.Text = "Organ Trail";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtChoice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTest;
+    }
+}
+
---
+++ OrganTrail/TheOrganTrail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrganTrail
+{
+    public partial class TheOrganTrail : Form
+    {
+        public TheOrganTrail()
+        {
+            InitializeComponent();
+
+        }
+
+        private void txtChoice_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Char ch = e.KeyChar; 
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+
+        }
+        private void TheOrganTrail_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            lblTest.Text = "Key Pressed: " + e.KeyChar;
+        }
+       
+        
+    }
+}
