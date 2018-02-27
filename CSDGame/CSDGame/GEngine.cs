f6455cc39957fb5c2bc19546e4cebef1b1e574a7
Noble115 <jimmyphamho@yahoo.com>
2018-02-27 15:03:23 -0600

    Test Game 1

---
+++ CSDGame/CSDGame/GEngine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    class GEngine
+    {
+        /*-------------------Members-------------------*/
+        private Graphics drewHandle;
+        private Thread renderThread;
+
+        /*-------------------Functions-------------------*/
+
+        public GEngine(Graphics g)
+        {
+            drewHandle = g;
+        }
+
+        public void init()
+        {
+            renderThread = new Thread(new ThreadStart(render));
+            renderThread.Start();
+        }
+
+        public void stop()
+        {
+            renderThread.Abort();
+        }
+
+        private void render()
+        {
+            while (true)
+            {
+                drewHandle.FillRectangle(new SolidBrush(Color.Aqua) , 0, 0, 1200, 700);
+            }
+        }
+    }
+}
---
+++ CSDGame/CSDGame/GameWind.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class GameWind : Form
+    {
+        private Game game = new Game();
+
+        public GameWind()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = canvas.CreateGraphics();
+            game.startGraphics(g);
+        }
+
+        private void GameWind_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            game.stopGame();
+        }
+    }
+}
